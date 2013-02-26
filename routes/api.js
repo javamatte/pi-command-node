424331e0676063497a68d7fa5aa601e982818bfe
Matt Everett <javamatte@gmail.com>
2013-02-26 00:05:38 -0800

    Updated Status app to do work, update api.js to start twiddling bits'

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,19 @@
  * rpi-gpio API implementation
  */
 
- // what pins are valid for control?
-var validOutputPins = [ 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ];
-var validInputPins = [ 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ];
+// what pins are valid for control?
+//var validOutputPins = [ 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ];
+//var validInputPins = [ 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26 ];
+var validOutputPins = [ 7 ];
+var validInputPins = [ 7 ];
+
+// setup gpio access (probably need root access for this)
+var gpio = require('rpi-gpio');
+var pin = null;
 
 exports.readPin = function(req, res) {
-  if (validInputPins.indexOf(parseInt(req.params.pin)) == -1) {
+  pin = parseInt(req.params.pin);
+  if (validInputPins.indexOf(pin) == -1) {
     res.json({ 
       operation: 'readPin',
       pin: req.params.pin,
@@ -21,14 +28,15 @@ exports.readPin = function(req, res) {
   
   res.json({ 
     operation: 'readPin', 
-    pin: req.params.pin,
+    pin: pin,
     result: 'success',
     value: 99
   });
 };
 
 exports.writePin = function(req, res) {
-  if (validOutputPins.indexOf(parseInt(req.params.pin)) == -1) {
+  pin = parseInt(req.params.pin);
+  if (validOutputPins.indexOf(pin) == -1) {
     res.json({
       operation: 'writePin',
       pin: req.params.pin,
@@ -52,12 +60,33 @@ exports.writePin = function(req, res) {
     return;
   }
   
-  // twiddle the gpio with rpi-gpio
+  if (value == 1) {
+	gpio.setup(pin, gpio.DIR_OUT, pinHigh);
+  } else {
+	gpio.setup(pin, gpio.DIR_OUT, pinLow);
+  }
   
   res.json({ 
     operation: 'writePin', 
-    pin: req.params.pin,
+    pin: pin,
     value: value,
     result: 'success'
   });
 };
+
+// pin control functions
+function pinHigh() {
+  gpio.write(pin, true, function(err) {
+    if (err) throw err;
+    console.log('Pin ' + pin + ' output set to High');
+  });
+}
+
+function pinLow() {
+  gpio.write(pin, false, function(err) {
+    if (err) throw err;
+    console.log('Pin ' + pin + ' output set to Low');
+  });
+}
+
+
